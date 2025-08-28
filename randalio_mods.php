091da643fe9e4239c0fc071556051cc9c500e9e4
Randal Pope <randalpope@Randals-MacBook-Air.local>
2025-08-28 15:50:01 -0400

    updated version

--- randalio_mods.php
+++ randalio_mods.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Randal.io Mods
  * Description: Theme Mods and Custom JavaScript for Randal.io
- * Version: 1.0.0
+ * Version: 2.0.0
  * Author: Randal Traicoff
  * License: Mine
  */
